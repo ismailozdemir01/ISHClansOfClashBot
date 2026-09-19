1d26df4c203cf90a00057c1fed97e32bd6f7d67b
ismailozdemir01 <ismailozdemir01@gmail.com>
2026-09-19 17:32:00 +0300

    feat: add bot/strategy/config.py

---
+++ bot/strategy/config.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+@dataclass(frozen=True,slots=True)
+class StrategyConfig:
+    min_score: float=.55
+    min_gold: int=100000
+    min_elixir: int=100000
+    min_dark: int=0
+    max_town_hall: int|None=None
