dd0e903b040cbba84765484280bfce3a6caf1391
ismailozdemir01 <ismailozdemir01@gmail.com>
2026-09-19 18:22:13 +0300

    Clean module entrypoint

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,6 +2,6 @@ import uvicorn
 from bot.app import create_app
 from bot.core.config import get_settings
 
-if __name__=="__main__":
-    settings=get_settings()
-    uvicorn.run(create_app(),host=settings.host,port=settings.port)
+if __name__ == "__main__":
+    settings = get_settings()
+    uvicorn.run(create_app(), host=settings.host, port=settings.port)
