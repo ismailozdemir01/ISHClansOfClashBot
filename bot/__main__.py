75559732c6a054cb8abcf308fb391c15c023b733
ismailozdemir01 <ismailozdemir01@gmail.com>
2026-09-19 17:34:58 +0300

    fix: complete bot/__main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,7 @@
+import uvicorn
 from bot.app import create_app
 from bot.core.config import get_settings
-import uvicorn
 
-if __name__ == "__main__":
-    s = get_settings()
-    uvicorn.run(create_app(), host=s.host, port=s.port)
+if __name__=="__main__":
+    settings=get_settings()
+    uvicorn.run(create_app(),host=settings.host,port=settings.port)
